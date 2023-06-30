4371fa1ee24a8c2c17442e14823dcf9628ae4b9d
viniciusopassos <viniciusopassos@hotmail.com>
2023-06-30 09:17:23 -0300

    feat: desafio1

---
+++ day1/desafio1.js
@@ -0,0 +1,30 @@
+let numberOne = 1;
+let stringOne = "1";
+let numberThirty = 30;
+let stringThirty = "30";
+let numberTen = 10;
+let stringTen = "10";
+
+if (numberOne == stringOne) {
+  console.log(
+    "As variáveis numberOne e stringOne tem o mesmo valor, mas tipos diferentes"
+  );
+} else {
+  console.log("As variáveis numberOne e stringOne não tem o mesmo valor");
+}
+
+if (numberThirty === stringThirty) {
+  console.log(
+    "As variáveis numberThirty e stringThirty tem o mesmo valor e mesmo tipo"
+  );
+} else {
+  console.log("As variáveis numberThirty e stringThirty não tem o mesmo tipo");
+}
+
+if (numberTen === stringTen) {
+  console.log(
+    "As variáveis numberoTen e stringTen tem o mesmo valor e mesmo tipo"
+  );
+} else {
+  console.log("As variáveis numberoTen e stringTen não tem o mesmo tipo");
+}
